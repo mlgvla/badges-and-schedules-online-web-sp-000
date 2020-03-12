c39ef11f0465cada272c7ff9398839bc73b7f607
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 15:32:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return"Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  batch_array = []
+  array.each do |name|
+    batch_array.push(badge_maker(name))
+  end
+  return batch_array
+end
